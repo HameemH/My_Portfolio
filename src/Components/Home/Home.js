1e2fbc0c65e7e3db8e87e6fca5039a1185d14090
HameemH <hameemhossain2@gmail.com>
2022-06-10 20:11:02 +0600

    about me

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,14 +14,17 @@ const Home = () => {
                 <div className='w-[15vw] hidden lg:block z-50 bg-cyan-900 fixed min-h-screen'>
                     <ul class="menu  p-4">
                         <li><a href='#banner' className='link-hover mx-auto text-white text-2xl active:text-orange-500'>Home</a></li>
+                        <li><a href='#about' className='target:text-orange-400 link-hover mx-auto text-white text-2xl active:text-orange-500'>About</a></li>
                         <li><a href='#projects' className='target:text-orange-400 link-hover mx-auto text-white text-2xl active:text-orange-500'>Projects</a></li>
                         <li><a href='#contact ' className='link-hover mx-auto text-white text-2xl active:text-orange-500'>Contact</a></li>
                     </ul>
                 </div>
 
                 <div className='lg:w-[85vw] lg:ml-auto'>
+                    
                     <Navbar></Navbar>
                     <Banner></Banner>
+                    <Aboutme></Aboutme>
                     <Projects></Projects>
                     <Contact></Contact>
                 </div>
